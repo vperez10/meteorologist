51a851ab909904d146a96b21777dbac9b6f10fdf
vperez10 <vperez@chicagobooth.edu>
2015-07-21 21:03:54 -0400

    street to weather complete!

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -16,25 +16,29 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
-def temp
-          require 'open-uri'
-          url = "https://api.forecast.io/forecast/5fa7d55c02984ea9bcad2898c1e155c4/#{@lat},#{@lng}"
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_street_address
 
-               parsed_data = JSON.parse(open(url).read)
-latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
-    end
+parsed_data = JSON.parse(open(url).read)
+@lat = parsed_data["results"][0]["geometry"]["location"]["lat"]
+@lng = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
+url = "https://api.forecast.io/forecast/d4496922b5aa2236a0a05a86812b20ed/#{@lat},#{@lng}"
+parsed_data = JSON.parse(open(url).read)
+    temperature = parsed_data["currently"]["temperature"]
+    summary = parsed_data["currently"]["summary"]
+    next_hour = parsed_data["minutely"]["summary"]
+    next_hours = parsed_data["hourly"]["summary"]
+    daily = parsed_data["daily"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = temperature
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = summary
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = next_hour 
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = next_hours
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = daily
 
     render("street_to_weather.html.erb")
   end
